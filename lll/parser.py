1dbe1fe3eadb8b728b634d182ba6761a0645f067
Chris Eskow <chriseskow@gmail.com>
2016-09-26 13:41:36 -0400

    Split program into modules

---
+++ lll/parser.py
@@ -0,0 +1,67 @@
+from collections import namedtuple
+
+Program = namedtuple("Program", ("expressions"))
+List = namedtuple("List", ("items"))
+Identifier = namedtuple("Identifier", ("name"))
+Integer = namedtuple("Integer", ("value"))
+Float = namedtuple("Float", ("value"))
+
+class Parser:
+    def __init__(self, tokens):
+        self.tokens = tokens
+
+    def parse(self):
+        return self.parse_program()
+
+    # program = expression* EOF
+    def parse_program(self):
+        expressions = []
+        while self.tokens:
+            if self.accept("EOF"):
+                break
+            expressions.append(self.parse_expression())
+        return Program(expressions)
+
+    # expression = IDENT | INT | FLOAT | list
+    def parse_expression(self):
+        token = self.accept("IDENT")
+        if token: return Identifier(token.string)
+
+        token = self.accept("INT")
+        if token: return Integer(int(token.string))
+
+        token = self.accept("FLOAT")
+        if token: return Float(float(token.string))
+
+        return self.parse_list()
+
+    # list = LPAREN expression* RPAREN
+    def parse_list(self):
+        self.expect("LPAREN")
+        expressions = []
+        while True:
+            try:
+                expressions.append(self.parse_expression())
+            except ParseError:
+                break
+        self.expect("RPAREN")
+        return List(expressions)
+
+    def accept(self, type):
+        token = self.tokens[0]
+        if token.type == type:
+            self.tokens = self.tokens[1:]
+            return token
+        else:
+            return None
+
+    def expect(self, type):
+        token = self.accept(type)
+        if token:
+            return token
+        else:
+            actual_type = self.tokens[0].type
+            raise ParseError("Expected token %s (got %s)" % (type, actual_type))
+
+class ParseError(RuntimeError):
+    pass
---
+++ lll/tokenizer.py
@@ -0,0 +1,37 @@
+from collections import namedtuple
+from re import compile
+
+Token = namedtuple("Token", ("type", "string"))
+
+class Tokenizer:
+    REGEXES = [
+        ("SPACE",   compile(r'\s+|#.*')),
+        ("LPAREN",  compile(r'\(')),
+        ("RPAREN",  compile(r'\)')),
+        ("FLOAT",   compile(r'[+\-]?[0-9]+\.[0-9]+')),
+        ("INT",     compile(r'[+\-]?[0-9]+')),
+        ("IDENT",   compile(r'[A-Za-z0-9~!@$%^&*\-_=+|<>?]*'))
+    ]
+
+    def __init__(self, code):
+        self.code = code
+
+    def tokenize(self):
+        tokens = []
+        while self.code:
+            token = self.next_token()
+            self.code = self.code[len(token.string):]
+            if token.type != "SPACE":
+                tokens.append(token)
+        tokens.append(Token("EOF", None))
+        return tokens
+
+    def next_token(self):
+        for type, regex in self.REGEXES:
+            match = regex.match(self.code)
+            if match:
+                return Token(type, match.group(0))
+        raise TokenizeError("Don't know the token: %s" % repr(self.code))
+
+class TokenizeError(RuntimeError):
+    pass
