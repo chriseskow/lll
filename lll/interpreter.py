39c7367d8c027ff62fb87c4812ed33a302982660
Chris Eskow <chriseskow@gmail.com>
2016-09-26 18:00:39 -0400

    Implement nested scopes on function calls

--- lll/interpreter.py
+++ lll/interpreter.py
@@ -1,80 +1,83 @@
 from collections import namedtuple
 from lll.parser import *
 
+Builtin = namedtuple("Builtin", ("attr_name"))
 Lambda = namedtuple("Lambda", ("params", "body"))
+
+class Scope:
+    def __init__(self, symbols={}, prev=None):
+        self.symbols = symbols
+        self.prev = prev
+
+    def lookup(self, name):
+        if name in self.symbols:
+            return self.symbols[name]
+        elif self.prev:
+            return self.prev.lookup(name)
+        else:
+            raise RuntimeError("Undefined identifier: %s" % name)
+
+    def define(self, name, value):
+        self.symbols[name] = value
 
 class Interpreter:
     BUILTINS = {
-        "def": "builtin_def",
-        "lambda": "builtin_lambda",
-        "if": "builtin_if",
-        "print": "builtin_print",
-        "=": "builtin_eq",
-        "<": "builtin_lt",
-        ">": "builtin_gt",
-        "+": "builtin_add",
-        "-": "builtin_sub",
-        "*": "builtin_mul"
+        "def": Builtin("builtin_def"),
+        "lambda": Builtin("builtin_lambda"),
+        "if": Builtin("builtin_if"),
+        "print": Builtin("builtin_print"),
+        "=": Builtin("builtin_eq"),
+        "<": Builtin("builtin_lt"),
+        ">": Builtin("builtin_gt"),
+        "+": Builtin("builtin_add"),
+        "-": Builtin("builtin_sub"),
+        "*": Builtin("builtin_mul")
     }
 
     def __init__(self, program):
         self.program = program
-        self.symbol_table = {}
+        self.scope = Scope(self.BUILTINS)
 
     def execute(self):
-        for expression in self.program.expressions:
-            self.eval_expression(expression)
+        for expr in self.program.expressions:
+            self.eval(expr)
 
-    def eval_expression(self, expression):
-        if isinstance(expression, String):
-            return expression.value
-        elif isinstance(expression, Integer):
-            return expression.value
-        elif isinstance(expression, Float):
-            return expression.value
-        elif isinstance(expression, Identifier):
-            if expression.name in self.symbol_table:
-                return self.symbol_table[expression.name]
-            else:
-                raise RuntimeError("Undefined identifier: %s" % expression.name)
-        elif isinstance(expression, List):
-            return self.eval_list(expression.items)
+    def eval(self, expr):
+        if isinstance(expr, (String, Integer, Float)):
+            return expr.value
+        elif isinstance(expr, Identifier):
+            return self.scope.lookup(expr.name)
+        elif isinstance(expr, List):
+            return self.call(expr.items)
         else:
-            raise RuntimeError("[BUG] Don't know how to evaluate: %s" % repr(expression))
+            raise RuntimeError("[BUG] Invalid expression: %s" % repr(expr))
 
-    def eval_list(self, items):
+    def call(self, items):
         if not items:
-            raise RuntimeError("Tried to call an empty list")
-        first = items[0]
-        if not isinstance(first, Identifier):
-            raise RuntimeError("Tried to call non-function")
-        return self.call_func(first.name, items[1:])
+            raise RuntimeError("Cannot call empty list")
 
-    def call_func(self, name, args):
-        if name in self.BUILTINS:
-            func = getattr(self, self.BUILTINS[name])
-            return func(args)
-        elif name in self.symbol_table:
-            func = self.symbol_table[name];
-            if not isinstance(func, Lambda):
-                raise RuntimeError("Trying to call non-function")
-            args = self.eval_args(args)
+        func = self.eval(items[0])
+        raw_args = items[1:]
+
+        if isinstance(func, Builtin):
+            callable = getattr(self, func.attr_name)
+            return callable(raw_args)
+        elif isinstance(func, Lambda):
+            args = self.eval_args(raw_args)
             if len(func.params) != len(args):
                 raise RuntimeError("Wrong number of arguments (expected %d, got %d)" % (len(func.params), len(args)))
-            for i, param in enumerate(func.params):
-                self.define_var(param, args[i])
+            locals = dict(zip(func.params, args))
+            self.scope = Scope(locals, self.scope)
             retval = None
             for expr in func.body:
-                retval = self.eval_expression(expr)
+                retval = self.eval(expr)
+            self.scope = self.scope.prev
             return retval
         else:
-            raise RuntimeError("Calling undefined method: %s" % name)
+            raise RuntimeError("Cannot call non-function: %s" % repr(func))
 
     def eval_args(self, args):
-        return [self.eval_expression(arg) for arg in args]
-
-    def define_var(self, name, value):
-        self.symbol_table[name] = value
+        return [self.eval(arg) for arg in args]
 
     def builtin_def(self, args):
         if len(args) != 2:
@@ -82,8 +85,8 @@ class Interpreter:
         if not isinstance(args[0], Identifier):
             raise RuntimeError("def called without an identifier as first argument")
         key = args[0].name
-        value = self.eval_expression(args[1])
-        self.define_var(key, value)
+        value = self.eval(args[1])
+        self.scope.define(key, value)
         return value
 
     def builtin_lambda(self, args):
@@ -95,11 +98,11 @@ class Interpreter:
     def builtin_if(self, args):
         if len(args) != 3:
             raise RuntimeError("if requires 3 arguments")
-        cond = self.eval_expression(args[0])
+        cond = self.eval(args[0])
         if (cond):
-            return self.eval_expression(args[1])
+            return self.eval(args[1])
         else:
-            return self.eval_expression(args[2])
+            return self.eval(args[2])
 
     def builtin_print(self, args):
         args = self.eval_args(args)
