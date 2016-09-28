985251848637eb033e0a029f3df123c0393ecccb
Chris Eskow <chriseskow@gmail.com>
2016-09-28 09:49:05 -0400

    Use 2-space indentation for shell scripts

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
 if which colordiff >/dev/null 2>&1; then
-    DIFF=colordiff
+  DIFF=colordiff
 else
-    DIFF=diff
+  DIFF=diff
 fi
 
 cd $(dirname ${BASH_SOURCE[0]})
 
 status=0
 for file in *.lll; do
-    expected=$(cat "$file" | sed -n 's/.*# *=> *\(.*\)/\1/p')
-    actual=$(../lll.py $file 2>&1)
+  expected=$(cat "$file" | sed -n 's/.*# *=> *\(.*\)/\1/p')
+  actual=$(../lll.py $file 2>&1)
 
-    if [ "$expected" = "$actual" ]; then
-        echo "OK: $file"
-    else
-        echo "=================================================="
-        echo "FAIL: $file"
-        $DIFF -u --label "$file (expected)" --label "$file (actual)" <(echo "$expected") <(echo "$actual")
-        echo "=================================================="
-        status=1
-    fi
+  if [ "$expected" = "$actual" ]; then
+    echo "OK: $file"
+  else
+    echo "=================================================="
+    echo "FAIL: $file"
+    $DIFF -u --label "$file (expected)" --label "$file (actual)" <(echo "$expected") <(echo "$actual")
+    echo "=================================================="
+    status=1
+  fi
 done
 
 exit $status
