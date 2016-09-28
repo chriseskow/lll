c07a181922ea6ca0eedc80284a4cff57e1de4de0
Chris Eskow <chriseskow@gmail.com>
2016-09-28 01:26:28 -0400

    Implement lexical scoping for closure support

--- lll/interpreter.py
+++ lll/interpreter.py
@@ -1,19 +1,20 @@
 from collections import namedtuple
 from lll.parser import *
 
-Builtin = namedtuple("Builtin", ("attr_name"))
-Lambda = namedtuple("Lambda", ("params", "body"))
+Operator = namedtuple("Operator", ("attr_name"))
+Builtin = namedtuple("Builtin", ("attr_name", "min_args", "max_args"))
+Lambda = namedtuple("Lambda", ("params", "body", "env"))
 
-class Scope:
-    def __init__(self, symbols={}, prev=None):
+class Env:
+    def __init__(self, symbols={}, outer=None):
         self.symbols = symbols
-        self.prev = prev
+        self.outer = outer
 
     def lookup(self, name):
         if name in self.symbols:
             return self.symbols[name]
-        elif self.prev:
-            return self.prev.lookup(name)
+        elif self.outer:
+            return self.outer.lookup(name)
         else:
             raise RuntimeError("Undefined identifier: %s" % name)
 
@@ -21,91 +22,106 @@ class Scope:
         self.symbols[name] = value
 
 class Interpreter:
-    BUILTINS = {
-        "def": Builtin("builtin_def"),
-        "lambda": Builtin("builtin_lambda"),
-        "if": Builtin("builtin_if"),
-        "print": Builtin("builtin_print"),
-        "=": Builtin("builtin_eq"),
-        "<": Builtin("builtin_lt"),
-        ">": Builtin("builtin_gt"),
-        "+": Builtin("builtin_add"),
-        "-": Builtin("builtin_sub"),
-        "*": Builtin("builtin_mul")
+    GLOBAL_SYMBOLS = {
+        "def": Operator("op_def"),
+        "lambda": Operator("op_lambda"),
+        "if": Operator("op_if"),
+        "print": Builtin("builtin_print", 0, None),
+        "=": Builtin("builtin_eq", 2, None),
+        "<": Builtin("builtin_lt", 2, 2),
+        ">": Builtin("builtin_gt", 2, 2),
+        "+": Builtin("builtin_add", 1, None),
+        "-": Builtin("builtin_sub", 1, None),
+        "*": Builtin("builtin_mul", 1, None)
     }
 
     def __init__(self, program):
         self.program = program
-        self.scope = Scope(self.BUILTINS)
 
     def execute(self):
+        env = Env(self.GLOBAL_SYMBOLS)
         for expr in self.program.expressions:
-            self.eval(expr)
+            self.eval(expr, env)
 
-    def eval(self, expr):
+    def eval(self, expr, env):
         if isinstance(expr, (String, Integer, Float)):
             return expr.value
         elif isinstance(expr, Identifier):
-            return self.scope.lookup(expr.name)
+            return env.lookup(expr.name)
         elif isinstance(expr, List):
-            return self.call(expr.items)
+
+            return self.eval_list(expr.items, env)
         else:
             raise RuntimeError("[BUG] Invalid expression: %s" % repr(expr))
 
-    def call(self, items):
+    def eval_list(self, items, env):
         if not items:
             raise RuntimeError("Cannot call empty list")
 
-        func = self.eval(items[0])
+        func = self.eval(items[0], env)
         raw_args = items[1:]
 
-        if isinstance(func, Builtin):
+        if isinstance(func, Operator):
             callable = getattr(self, func.attr_name)
-            return callable(raw_args)
-        elif isinstance(func, Lambda):
-            args = self.eval_args(raw_args)
-            if len(func.params) != len(args):
-                raise RuntimeError("Wrong number of arguments (expected %d, got %d)" % (len(func.params), len(args)))
-            locals = dict(zip(func.params, args))
-            self.scope = Scope(locals, self.scope)
-            retval = None
-            for expr in func.body:
-                retval = self.eval(expr)
-            self.scope = self.scope.prev
-            return retval
+            return callable(raw_args, env)
+        elif isinstance(func, (Builtin, Lambda)):
+            args = self.map_eval(raw_args, env)
+            return self.apply(func, args)
         else:
             raise RuntimeError("Cannot call non-function: %s" % repr(func))
 
-    def eval_args(self, args):
-        return [self.eval(arg) for arg in args]
+    def apply(self, func, args):
+        if isinstance(func, Builtin):
+            num_args = len(args)
+            if num_args < func.min_args:
+                raise RuntimeError(
+                    "Too few arguments for builtin (min %d, but got %d)" %
+                    (func.min_args, num_args))
+            if func.max_args is not None and num_args > func.max_args:
+                raise RuntimeError(
+                    "Too many arguments for builtin (max %d, but got %d)" %
+                    (func.max_args, num_args))
+            callable = getattr(self, func.attr_name)
+            return callable(*args)
+        elif isinstance(func, Lambda):
+            if len(func.params) != len(args):
+                raise RuntimeError(
+                    "Wrong number of arguments (expected %d, got %d)" %
+                    (len(func.params), len(args)))
+            symbols = dict(zip(func.params, args))
+            env = Env(symbols, func.env)
+            retval = None
+            for expr in func.body:
+                retval = self.eval(expr, env)
+            return retval
 
-    def builtin_def(self, args):
+    def map_eval(self, args, env):
+        return [self.eval(arg, env) for arg in args]
+
+    def op_def(self, args, env):
         if len(args) != 2:
             raise RuntimeError("def called with wrong number of arguments")
-        if not isinstance(args[0], Identifier):
+        (ident, value_expr) = args
+        if not isinstance(ident, Identifier):
             raise RuntimeError("def called without an identifier as first argument")
-        key = args[0].name
-        value = self.eval(args[1])
-        self.scope.define(key, value)
+        key = ident.name
+        value = self.eval(value_expr, env)
+        env.define(key, value)
         return value
 
-    def builtin_lambda(self, args):
+    def op_lambda(self, args, env):
         if len(args) < 2:
             raise RuntimeError("lambda called with wrong number of arguments")
         params = self.require_param_list(args[0])
-        return Lambda(params, args[1:])
+        return Lambda(params, args[1:], env)
 
-    def builtin_if(self, args):
+    def op_if(self, args, env):
         if len(args) != 3:
             raise RuntimeError("if requires 3 arguments")
-        cond = self.eval(args[0])
-        if (cond):
-            return self.eval(args[1])
-        else:
-            return self.eval(args[2])
+        (cond, then_expr, else_expr) = args
+        return self.eval(then_expr if self.eval(cond, env) else else_expr, env)
 
-    def builtin_print(self, args):
-        args = self.eval_args(args)
+    def builtin_print(self, *args):
         for arg in args:
             if isinstance(arg, (str, int, long, float)):
                 print(arg)
@@ -114,33 +130,19 @@ class Interpreter:
             else:
                 raise RuntimeError("[BUG] Don't know how to print: %s" % repr(arg))
 
-    def builtin_eq(self, args):
-        args = self.eval_args(args)
-        self.require_nonempty_numeric_args(args)
-        if len(args) == 1:
-            raise RuntimeError("Need more than one argument")
+    def builtin_eq(self, *args):
         return int(args.count(args[0]) == len(args))
 
-    def builtin_lt(self, args):
-        if len(args) != 2:
-            raise RuntimeError("Need two arguments")
-        args = self.eval_args(args)
-        self.require_nonempty_numeric_args(args)
-        return int(args[0] < args[1])
+    def builtin_lt(self, x, y):
+        return int(x < y)
 
-    def builtin_add(self, args):
-        args = self.eval_args(args)
-        self.require_nonempty_numeric_args(args)
-        return reduce(lambda sum, n: sum + n, args, 0)
+    def builtin_add(self, *args):
+        return sum(args)
 
-    def builtin_sub(self, args):
-        args = self.eval_args(args)
-        self.require_nonempty_numeric_args(args)
+    def builtin_sub(self, *args):
         return reduce(lambda sum, n: sum - n, args[1:], args[0])
 
-    def builtin_mul(self, args):
-        args = self.eval_args(args)
-        self.require_nonempty_numeric_args(args)
+    def builtin_mul(self, *args):
         return reduce(lambda product, n: product * n, args, 1)
 
     def require_param_list(self, list):
@@ -152,9 +154,3 @@ class Interpreter:
                 raise RuntimeError("lambda called with non-identifier param")
             params.append(item.name)
         return params
-
-    def require_nonempty_numeric_args(self, args):
-        if not args:
-            raise RuntimeError("Argument list is empty")
-        if any([type(arg) not in (int, long, float) for arg in args]):
-            raise RuntimeError("Need numeric arguments")
